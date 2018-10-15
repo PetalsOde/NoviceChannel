b8fb401a48f1af5ba4d60c891f00c0988bccc7ea
Newbie Coder <40880914+Newbie-Coder-1105@users.noreply.github.com>
2018-10-15 12:13:10 +0530

    Add files via upload

---
+++ LonGrid.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+class NewLonGrid
+{
+	public :
+				
+		float new_lon_grid(float *h_new_lon, int &NLON, float del_lon);
+};
+
