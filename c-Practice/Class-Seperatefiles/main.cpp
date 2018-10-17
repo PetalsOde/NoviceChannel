af557b63a9f654f230738e2e3654cf293049ee43
Newbie Coder <40880914+Newbie-Coder-1105@users.noreply.github.com>
2018-10-17 14:44:32 +0530

    main program

---
+++ c-Practice/Class-Seperatefiles/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <math.h>
+
+using namespace std;
+#include "Books.h"
+#include "Tria.h"
+#include "Shape.h"
+
+int main()
+{ 
+	Books book1;
+	Books book2;
+	book1.setBookId(1234);
+	book2.setBookId(6789);
+	cout<<book1.getBookId(67)<<endl;
+	cout<<book2.getBookId(80)<<endl;
+	
+	Triangle triangle1;
+	Triangle triangle2;
+	triangle1.setBase(45);
+	triangle1.setHeight(20);
+	triangle2.setBase(24);
+	triangle2.setHeight(25);
+	
+	
+	cout<<triangle1.getTriangleperimeter()<<endl;	
+	cout<<triangle2.getTriangleperimeter()<<endl;
+	cout<<triangle1.getTrianglearea()<< "= qwer"<<endl;
+	float qwer;
+	qwer = triangle1.getTrianglearea();
+	cout<<"qwer = "<< qwer<<endl;
+	
+	Rectangle rect1;
+	rect1.setBase(12);
+	rect1.setHeight(12);
+	
+	cout<<rect1.getRectanglearea()<<endl;
+//	cout<<rect1.getRectangleperimeter()<<endl;
+	return 0;
+}
