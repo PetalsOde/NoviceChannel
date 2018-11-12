88e6f8edbc3bec63752b0925f52ee9cc6a6765f8
Newbie Coder <40880914+Newbie-Coder-1105@users.noreply.github.com>
2018-11-12 15:35:28 +0530

    Update data_interface.h

--- c-Practice/4D-Pointer/data_interface.h
+++ c-Practice/4D-Pointer/data_interface.h
@@ -12,5 +12,5 @@ class Data_handle_read
 		void data_4D_read(size_t &I_RECS ,size_t &I_LVLS, size_t &I_LATS,size_t &I_LONS, float *zpos, 
 					float *ypos, float *xpos, double *tpos,	short *****var) ;
 					
-		void data_2D_read(size_t &I_LATS,size_t &I_LONS, 	float *ypos, float *xpos, short ***var) ;
+		void data_2D_read(size_t &I_LATS,size_t &I_LONS, float *ypos, float *xpos, short ***var) ;
 };
