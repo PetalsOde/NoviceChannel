85e777e6550e72ab45d75fa24a97ac47590978d8
Newbie Coder <40880914+Newbie-Coder-1105@users.noreply.github.com>
2018-11-19 12:34:12 +0530

    Create QuatDpointer.h

---
+++ c-Practice/dynamic_pointer/separate_ex/QuatDpointer.h
@@ -0,0 +1,10 @@
+#include <iostream>
+
+
+class QuatDpointer
+{
+        public :
+                QuatDpointer () ;
+                void data_read(size_t &nt, size_t &nz, size_t &ny, size_t &nx, float ****var );
+};
+
