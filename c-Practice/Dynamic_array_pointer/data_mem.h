221883fe86647ffe2e063ae7f8b32d548bcd76d7
Newbie Coder <40880914+Newbie-Coder-1105@users.noreply.github.com>
2018-11-29 15:42:23 +0530

    Create data_mem.h

---
+++ c-Practice/Dynamic_array_pointer/data_mem.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+class Data_mem 
+{
+	public:
+		template<class Type>
+			int Create4D_Array(Type *****pResult, int a, int b, int c, int d)
+			{
+				Type ****p = new Type ***[a];
+				for(int i = 0; i < a; i++)
+				{
+					p[i] = new Type **[b];
+					for(int j = 0; j < b; j++)
+					{
+						p[i][j] = new Type *[c];
+						for(int k =0; k < c; k++)
+						{
+							p[i][j][k] = new Type [d];
+						}
+					}
+					
+				}
+				*pResult = p;
+				return  0;
+			} 
+			
+		void data_storey(int &w, int &x, int &y, int &z, short *****var);
+			
+};
